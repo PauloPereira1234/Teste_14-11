ddc5eac9ebd5adea08f264ebd1ebf1a0aedd787a
PauloPereira1234 <57690133+PauloPereira1234@users.noreply.github.com>
2019-11-15 17:34:27 +0000

    Update Person.js

--- sonarqube-scanner/src/javascript/Person.js
+++ sonarqube-scanner/src/javascript/Person.js
@@ -17,7 +17,7 @@ var a = NaN;
 if (Number.isNaN(a)) {  // Noncompliant; always false
   console.log("a is not a number");  // this is dead code
 }
-if (a !== NaN) { // Noncompliant; always true
+if (Number.isNaN(a)) { // Noncompliant; always true
   console.log("a is not NaN"); // this statement is not necessarily true
 }
 
