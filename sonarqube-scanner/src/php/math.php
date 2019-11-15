3d9477b90a58333508c3745c12c74b6905243089
PauloPereira1234 <57690133+PauloPereira1234@users.noreply.github.com>
2019-11-15 18:23:47 +0000

    Update math.php

--- sonarqube-scanner/src/php/math.php
+++ sonarqube-scanner/src/php/math.php
@@ -151,3 +151,64 @@ class PhpUnderControl_Example_Math
     /**
      * Simple copy for cpd detection.
      */
+    public function complex($v1, $v2)
+    {
+        $v3 = $v1 / ($v2 + $v1);
+        if ($v3 > 14)
+        {
+            $v4 = 0;
+            for ($i = 0; $i < $v3; $i++)
+            {
+                $v4 += ($v2 * $i);
+            }
+        }
+        $v5 = ($v4 < $v3 ? ($v3 - $v4) : ($v4 - $v3));
+
+        $v6 = ($v1 * $v2 * $v3 * $v4 * $v5);
+
+        $d = array($v1, $v2, $v3, $v4, $v5, $v6);
+
+        $v7 = 1;
+        for ($i = 0; $i < $v6; $i++)
+        {
+            shuffle( $d );
+            $v7 = $v7 + $i * end( $d );
+        }
+
+        $v8 = $v7;
+        foreach ( $d as $x )
+        {
+            $v8 *= $x;
+        }
+        
+        $v3 = $v1 / ($v2 + $v1);
+        if ($v3 > 14)
+        {
+            $v4 = 0;
+            for ($i = 0; $i < $v3; $i++)
+            {
+                $v4 += ($v2 * $i);
+            }
+        }
+        $v5 = ($v4 < $v3 ? ($v3 - $v4) : ($v4 - $v3));
+
+        $v6 = ($v1 * $v2 * $v3 * $v4 * $v5);
+
+        $d = array($v1, $v2, $v3, $v4, $v5, $v6);
+
+        $v7 = 1;
+        for ($i = 0; $i < $v6; $i++)
+        {
+            shuffle( $d );
+            $v7 = $v7 + $i * end($d);
+        }
+
+        $v8 = $v7;
+        foreach ( $d as $x )
+        {
+            $v8 *= $x;
+        }
+
+        return $v8;
+    }
+}
